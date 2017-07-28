LineageOS/android_packages_apps_Trebuchet

Trebuchet: Rename settings provider
Rename in order to avoid conflicts with AOSP launcher and
other launchers based on it.

Change-Id: Ib44f96add26d0f60d08e642044b1cde35f489cd3

src/com/android/launcher3/BaseContainerView.java
@@ -26,7 +26,7 @@ import android.view.ViewStub;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.android.launcher3.settings.SettingsProvider;
+import com.android.trebuchet.settings.SettingsProvider;
 
 /**
  * A base container view, which supports resizing.

src/com/android/launcher3/DeviceProfile.java
@@ -31,9 +31,9 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import com.android.launcher3.settings.SettingsProvider;
 
 import com.android.launcher3.allapps.AllAppsContainerView;
+import com.android.trebuchet.settings.SettingsProvider;
 
 public class DeviceProfile {
     public final InvariantDeviceProfile inv;

src/com/android/launcher3/DynamicGridSizeFragment.java
@@ -43,7 +43,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
-import com.android.launcher3.settings.SettingsProvider;
+import com.android.trebuchet.settings.SettingsProvider;
 
 public class DynamicGridSizeFragment extends Fragment
         implements NumberPicker.OnValueChangeListener, Dialog.OnDismissListener {

src/com/android/launcher3/FolderPagedView.java
@@ -31,8 +31,8 @@ import android.view.animation.OvershootInterpolator;
 import com.android.launcher3.FocusHelper.PagedFolderKeyEventListener;
 import com.android.launcher3.PageIndicator.PageMarkerResources;
 import com.android.launcher3.Workspace.ItemOperator;
-import com.android.launcher3.settings.SettingsProvider;
 import com.android.launcher3.util.Thunk;
+import com.android.trebuchet.settings.SettingsProvider;
 
 import java.util.ArrayList;
 import java.util.HashMap;

src/com/android/launcher3/LauncherModel.java
@@ -64,9 +64,9 @@ import com.android.launcher3.util.CursorIconInfo;
 import com.android.launcher3.util.LongArrayMap;
 import com.android.launcher3.util.ManagedProfileHeuristic;
 import com.android.launcher3.util.Thunk;
-import cyanogenmod.providers.CMSettings;
+import com.android.trebuchet.settings.SettingsProvider;
 
-import com.android.launcher3.settings.SettingsProvider;
+import cyanogenmod.providers.CMSettings;
 
 import java.lang.ref.WeakReference;
 import java.net.URISyntaxException;

src/com/android/launcher3/Utilities.java
@@ -58,7 +58,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.Toast;
 
-import com.android.launcher3.settings.SettingsProvider;
+import com.android.trebuchet.settings.SettingsProvider;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;

src/com/android/launcher3/allapps/AllAppsGridAdapter.java
@@ -45,8 +45,8 @@ import com.android.launcher3.ProtectedComponentsHelper;
 import com.android.launcher3.R;
 import com.android.launcher3.RemoteFolderManager;
 import com.android.launcher3.Utilities;
-import com.android.launcher3.settings.SettingsProvider;
 import com.android.launcher3.util.Thunk;
+import com.android.trebuchet.settings.SettingsProvider;
 
 import java.util.HashMap;
 import java.util.List;

src/com/android/launcher3/config/ProviderConfig.java
@@ -18,5 +18,5 @@ package com.android.launcher3.config;
 
 public class ProviderConfig {
 
-    public static final String AUTHORITY = "com.android.launcher3.settings".intern();
+    public static final String AUTHORITY = "com.android.trebuchet.settings".intern();
 }

archos-sa/ArchosLauncher

src/com/android/launcher3/BaseContainerView.java
@@ -26,7 +26,7 @@ import android.view.ViewStub;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.android.launcher3.settings.SettingsProvider;
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 /**
  * A base container view, which supports resizing.

src/com/android/launcher3/DeviceProfile.java
@@ -31,9 +31,9 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import com.android.launcher3.settings.SettingsProvider;
 
 import com.android.launcher3.allapps.AllAppsContainerView;
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 public class DeviceProfile {
     public final InvariantDeviceProfile inv;

src/com/android/launcher3/DynamicGridSizeFragment.java
@@ -43,7 +43,8 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
-import com.android.launcher3.settings.SettingsProvider;
+
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 public class DynamicGridSizeFragment extends Fragment
         implements NumberPicker.OnValueChangeListener, Dialog.OnDismissListener {

src/com/android/launcher3/FolderPagedView.java
@@ -31,8 +31,8 @@ import android.view.animation.OvershootInterpolator;
 import com.android.launcher3.FocusHelper.PagedFolderKeyEventListener;
 import com.android.launcher3.PageIndicator.PageMarkerResources;
 import com.android.launcher3.Workspace.ItemOperator;
-import com.android.launcher3.settings.SettingsProvider;
 import com.android.launcher3.util.Thunk;
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 import java.util.ArrayList;
 import java.util.HashMap;

src/com/android/launcher3/LauncherModel.java
@@ -64,9 +64,9 @@ import com.android.launcher3.util.CursorIconInfo;
 import com.android.launcher3.util.LongArrayMap;
 import com.android.launcher3.util.ManagedProfileHeuristic;
 import com.android.launcher3.util.Thunk;
-import cyanogenmod.providers.CMSettings;
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
-import com.android.launcher3.settings.SettingsProvider;
+import cyanogenmod.providers.CMSettings;
 
 import java.lang.ref.WeakReference;
 import java.net.URISyntaxException;

src/com/android/launcher3/Utilities.java
@@ -58,7 +58,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.Toast;
 
-import com.android.launcher3.settings.SettingsProvider;
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;

src/com/android/launcher3/allapps/AllAppsGridAdapter.java
@@ -45,8 +45,8 @@ import com.android.launcher3.ProtectedComponentsHelper;
 import com.android.launcher3.R;
 import com.android.launcher3.RemoteFolderManager;
 import com.android.launcher3.Utilities;
-import com.android.launcher3.settings.SettingsProvider;
 import com.android.launcher3.util.Thunk;
+import com.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 import java.util.HashMap;
 import java.util.List;

src/com/android/launcher3/config/ProviderConfig.java
@@ -18,5 +18,5 @@ package com.android.launcher3.config;
 
 public class ProviderConfig {
 
-    public static final String AUTHORITY = "com.android.launcher3.settings".intern();
+    public static final String AUTHORITY = "com.cyanogenmod.trebuchet.settings".intern();
 }